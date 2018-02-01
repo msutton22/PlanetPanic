9d6543c0d37583e63aeef889c23d99637d6b9e6f
Unknown <ms9642@nyu.edu>
2018-01-31 20:51:41 -0500

    Added spaceship and left and right behavior

---
+++ Assets/SpaceShipBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipBehavior : MonoBehaviour {
+	public float speed;
+	SpriteRenderer spaceship;
+	// Use this for initialization
+	void Start () {
+		spaceship = this.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			this.GetComponent<Transform> ().Translate (new Vector3 (speed, 0));	
+		}
+
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			this.GetComponent<Transform> ().Translate (new Vector3 (-speed, 0));		
+		}
+	}
+}
