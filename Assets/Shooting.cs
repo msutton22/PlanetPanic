ff3e8a9c31d9ada255e63385291172fb232b6719
Unknown <ms9642@nyu.edu>
2018-02-01 11:18:26 -0500

    Shooting!

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour {
 
+	public float fireRate = 0;
+	public float damage = 10;
+   // public LayerMask notToHit;
+	float timeToShoot = 0;
+	Transform firePoint;
 	// Use this for initialization
 	void Start () {
-		
+		firePoint = transform.FindChild ("Fire Point");
 	}
 	
 	// Update is called once per frame
--- Assets/SpaceShipBehavior.cs
+++ Assets/SpaceShipBehavior.cs
@@ -5,13 +5,37 @@ using UnityEngine;
 public class SpaceShipBehavior : MonoBehaviour {
 	public float speed;
 	SpriteRenderer spaceship;
+
+	public GameObject projectilePrefabs;
+	private List <GameObject> Projectiles = new List<GameObject> ();
+	private float projectileVelocity;
 	// Use this for initialization
 	void Start () {
 		spaceship = this.GetComponent<SpriteRenderer> ();
+		projectileVelocity = 5;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (Input.GetButtonDown ("Jump")) 
+		{
+			GameObject bullet = (GameObject)Instantiate (projectilePrefabs, transform.position, Quaternion.identity);
+			Projectiles.Add (bullet);
+		}
+
+		for (int i = 0; i < Projectiles.Count; i++) {
+			GameObject goBullet = Projectiles [i];
+			if (goBullet != null) {
+				goBullet.transform.Translate (new Vector3 (0, 1) * Time.deltaTime * projectileVelocity);
+				Vector3 bulletScreenPos = Camera.main.WorldToScreenPoint (goBullet.transform.position);
+				if (bulletScreenPos.y >= Screen.height) {
+					DestroyObject (goBullet);
+					Projectiles.Remove (goBullet);
+				}
+			}
+		}
+
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			this.GetComponent<Transform> ().Translate (new Vector3 (speed, 0));	
 		}
