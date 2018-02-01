a30004551f6ae74e5f61ba3dae299a55582eebc6
Unknown <ms9642@nyu.edu>
2018-02-01 11:37:55 -0500

    Added comments to code

--- Assets/SpaceShipBehavior.cs
+++ Assets/SpaceShipBehavior.cs
@@ -20,29 +20,29 @@ public class SpaceShipBehavior : MonoBehaviour {
 
 		if (Input.GetButtonDown ("Jump"))  //if the spacebar is pressed 
 		{
-			GameObject bullet = (GameObject)Instantiate (projectilePrefabs, transform.position, Quaternion.identity);
-			Projectiles.Add (bullet);
+			GameObject bullet = (GameObject)Instantiate (projectilePrefabs, transform.position, Quaternion.identity); //create a projectile object in the current position
+			Projectiles.Add (bullet); //add actual projectial or bullet to scene
 		}
 
-		for (int i = 0; i < Projectiles.Count; i++) {
-			GameObject goBullet = Projectiles [i];
-			if (goBullet != null) {
-				goBullet.transform.Translate (new Vector3 (0, 1) * Time.deltaTime * projectileVelocity);
-				Vector3 bulletScreenPos = Camera.main.WorldToScreenPoint (goBullet.transform.position);
-				if (bulletScreenPos.y >= Screen.height) {
-					DestroyObject (goBullet);
-					Projectiles.Remove (goBullet);
+		for (int i = 0; i < Projectiles.Count; i++) { //loop to see how many projectiles are in the scene
+			GameObject goBullet = Projectiles [i]; //going through list of projectiles to see number of projectiles
+			if (goBullet != null) { //checking if goBullet is null
+				goBullet.transform.Translate (new Vector3 (0, 1) * Time.deltaTime * projectileVelocity); //Projectiles moving at given velocity over time
+				Vector3 bulletScreenPos = Camera.main.WorldToScreenPoint (goBullet.transform.position); // Finding the position of the bullet
+				if (bulletScreenPos.y >= Screen.height) { //checking to see if the bullet has gone out of the screen
+					DestroyObject (goBullet); //will get rid of bullet if bullet leaves screen
+					Projectiles.Remove (goBullet); //Projectile will be removed 
 				}
 			}
 		}
 
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			this.GetComponent<Transform> ().Translate (new Vector3 (speed, 0));	
+		if (Input.GetKey (KeyCode.RightArrow)) { //if right arrow is pressed
+			this.GetComponent<Transform> ().Translate (new Vector3 (speed, 0));	//spaceship will move right at a speed dictated elsewhere
 		}
 
 
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			this.GetComponent<Transform> ().Translate (new Vector3 (-speed, 0));		
+		if (Input.GetKey (KeyCode.LeftArrow)) { //if left arrow is pressed
+			this.GetComponent<Transform> ().Translate (new Vector3 (-speed, 0));	//spaceship willl move left at a negative speed dictated elsewhere
 		}
 	}
 }
