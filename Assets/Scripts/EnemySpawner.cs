f302350d27f2fa3250217230b7b7679e58a6a3b8
Unknown <ms9642@nyu.edu>
2018-02-06 11:53:10 -0500

    Commenting Code

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
-	public GameObject enemy;
-	float randX;
-	float randY;
-	Vector2 whereToSpawn;
-	public float spawnRate = 2f;
-	float nextSpawn = 0.0f;
+	public GameObject enemy; //creating game object from prefab
+	float randX; //random variable for x
+	float randY;  //random variable for y
+	Vector2 whereToSpawn; //Vector to show where the enemy will spawn
+	public float spawnRate = 2f; //the rate at which enemies will spawn
+	float nextSpawn = 0.0f;  //keeping track of time to next enemy
 
 	// Use this for initialization
 	void Start () {
@@ -17,12 +17,12 @@ public class EnemySpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.time > nextSpawn) {
-			nextSpawn = Time.time + spawnRate;
-			randX = Random.Range (-8.4f, 8.4f);
-			randY = Random.Range (4f, -1f);
-			whereToSpawn = new Vector2 (randX, randY);
-			Instantiate (enemy, whereToSpawn, Quaternion.identity);
+		if (Time.time > nextSpawn) { //checking if new enemy should be spawned
+			nextSpawn = Time.time + spawnRate; // The time it takes for a new enemy to spawn
+			randX = Random.Range (-8.4f, 8.4f); //finding a random x value in a range
+			randY = Random.Range (4f, -1f); //finding a random y value in a range
+			whereToSpawn = new Vector2 (randX, randY); //will spawn enemy in random range number coordinates
+			Instantiate (enemy, whereToSpawn, Quaternion.identity); //Enemy will be on the screen from prefab
 		}
 	}
 }
