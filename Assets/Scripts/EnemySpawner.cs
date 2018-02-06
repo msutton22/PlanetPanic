5c9819dc80b5200451d6a7fafd78d714312e7f2f
Unknown <ms9642@nyu.edu>
2018-02-06 11:47:33 -0500

    Randomly Generated Enemies

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject enemy;
+	float randX;
+	float randY;
+	Vector2 whereToSpawn;
+	public float spawnRate = 2f;
+	float nextSpawn = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextSpawn) {
+			nextSpawn = Time.time + spawnRate;
+			randX = Random.Range (-8.4f, 8.4f);
+			randY = Random.Range (4f, -1f);
+			whereToSpawn = new Vector2 (randX, randY);
+			Instantiate (enemy, whereToSpawn, Quaternion.identity);
+		}
+	}
+}
