ee9a33a3f2644945ff48470bdd641c242cf06d5f
Unknown <ms9642@nyu.edu>
2018-02-20 11:50:40 -0500

    Beginning Scene Created

---
+++ Assets/Scripts/BeginningToNextScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BeginningToNextScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+public void PlayGame() {
+	SceneManager.LoadScene (1); //if the button is pressed, go to the game
+}
+
+}
